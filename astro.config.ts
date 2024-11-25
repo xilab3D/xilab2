eb0072733684bcd2c6ea24b1105125928d1381ce
xilab3D <ngardan@xilab.tech>
2024-11-25 08:40:51 +0100

    Update astro.config.ts

--- astro.config.ts
+++ astro.config.ts
@@ -22,6 +22,7 @@ const whenExternalScripts = (items: (() => AstroIntegration) | (() => AstroInteg
   hasExternalScripts ? (Array.isArray(items) ? items.map((item) => item()) : [items()]) : [];
 
 export default defineConfig({
+  site: 'https://xilab2.github.io/',
   output: 'static',
 
   integrations: [
